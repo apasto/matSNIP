61e93db17bbf2f6e916c5fd14fb0c11a35253a0f
Alberto Pastorutti <albert.schaferle@gmail.com>
2018-09-04 10:48:10 +0200

    given two maps, 'Big' and 'Small', crop 'Big' to the extent of 'Small'

---
+++ +SNIP/SmallCrop.m
@@ -0,0 +1,89 @@
+function CropBigArr = SmallCrop(BigLat,BigLon,BigArr,SmallLat,SmallLon)
+%SmallCrop Given two maps, 'Big' and 'Small', crop 'Big' to the extent of 'Small'
+%   Typical usage: a 'Small' map which used 'Big' map as input,
+%   but underwent projection/unprojection and/or removal of edge effects.
+%   To perform direct comparison on data, we need to crop 'Big'
+%   to the extent of small.
+%   'Big' is given as Lat, Lon vectors and a Lat,Lon array,
+%   while only the Lat, Lon vectors of 'Small' are needed.
+%   Input checks:
+%    - are all the Lat, Lon vectors northward, eastward sorted?
+%          (i.e. increasing)
+%    - is the step between elements constant?
+%    - are the steps of 'Big' and 'Small' the same?
+%    - do 'Big' and 'Small' share the same origin?
+%          (they may have the same step, but the grids may not coincide)
+%
+% Syntax: CropBigArr = SmallCrop(BigLat,BigLon,BigArr,SmallLat,SmallLon,SmallArr)
+%
+% Input:
+%    BigLat   : 'Big' latitude vector
+%    BigLon   : 'Big' longitude vector
+%    BigArr   : 'Big' map-array, Lat x Lon
+%    SmallLat : 'Small' latitude vector
+%    SmallLon : 'Small' longitude vector
+%
+% Output:
+%    CropBigArr : 'Big' map-array, cropped to the extent defined by
+%                 SmallLat and SmallLon
+%
+% 2018, Alberto Pastorutti
+
+%% check input
+% embedded functions, to avoid writing everyting four times
+% using inputname(argNumber) to print out variable names in errors
+AssertInputsTogether(BigLat,SmallLat,inputname(1),inputname(4));
+AssertInputsTogether(BigLat,SmallLat,inputname(2),inputname(5));
+
+%% find crop indices
+CropEdge_W = find(BigLon==min(SmallLon),1);
+CropEdge_E = find(BigLon==max(SmallLon),1);
+CropEdge_S = find(BigLat==min(SmallLat),1);
+CropEdge_N = find(BigLat==max(SmallLat),1);
+
+%% crop
+CropBigArr = BigArr(CropEdge_S:CropEdge_N,CropEdge_W:CropEdge_E);
+
+%% functions: perform input checks
+    function AssertInputsTogether(BigIn,SmallIn,BigInVarName,SmallInVarName)
+        % this calls AssertInputsEach for single-input-tests
+        % and performs Big-Small comparisons
+        
+        % non-comparison tests: increasing coordinates, constant step
+        StepBig   = AssertInputsEach(BigIn,BigInVarName);
+        StepSmall = AssertInputsEach(SmallIn,SmallInVarName);
+        
+        % Big-Small comparison: same step?
+        assert(StepBig==StepSmall,['Non equal steps: ',...
+            BigInVarName,'=',num2str(StepBig),' and '...
+            SmallInVarName,'=',num2str(StepSmall)]);
+        
+        % Big-Small comparison: same origin?
+        % strategy:
+        %  1) crop biglat/lon to length of smallat/lon
+        %  2) subtract small to big_cropped
+        %  3) mod(result of item 2, step)
+        %  4) all zeros? same origin
+        %  5) otherwise, different origin
+        BigCrop = BigIn(1:length(SmallIn));
+        assert(all(mod(SmallIn-BigCrop,StepBig)==0),...
+            ['Different origin, non coinciding grids defined by: ',...
+            BigInVarName,' and ',SmallInVarName]);
+    end
+
+    function VectStep = AssertInputsEach(CoordVect,InVarName)
+        % called inside AssertInputsTogether
+        % instead of writing non-comparison tests twice (for Big and Small)
+        DiffCoordVect = diff(CoordVect);
+        % 1) are coordinates increasing
+        assert(all(DiffCoordVect>0),...
+            ['Non increasing coordinate vector: ',InVarName]);
+        % 2) are the steps constant?
+        assert(all(DiffCoordVect==DiffCoordVect(1)),...
+            ['Non constant-step coordinate vector: ',InVarName]);
+        % write out step
+        VectStep = DiffCoordVect(1);
+    end
+
+end
+
